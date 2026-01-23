430c8272e39989e35be12b2ee3f1a18e9800160c
phamc8-glitch <phamc8@vcu.edu>
2026-01-23 16:27:57 -0500

    Script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+// Wars
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+const catbutton_KEYS = ['shift'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+const catbuttonKeys = document.querySelectorAll('.key.catbutton');
+
+// Select all keys
+const keys = document.querySelectorAll('.key');
+
+// -- Listeners -- //
+// Add an event listener to all keys
+keys.forEach((key) => {
+  key.addEventListener('click', () => playNote(key));
+});
+
+// -- Handlers -- //
+function playNote(key) {
+    // Get the note's value
+    const noteAudio = document.getElementById(key.dataset.note);
+ 
+  // Reset the note's current time to 0
+  noteAudio.currentTime = 0;
+  // Play the note
+  noteAudio.play();
+
+  // Add a class, active, for styling purposes
+  key.classList.add('active');
+
+  // Listen for when the sound has ended and remove active class
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
+}
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+  const catbuttonKeyIndex = Catbutton_KEYS.indexOf(key);
+
+  // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+  
+if (catbuttonKeyIndex > -1) {
+    playNote(catmeow[catbuttonKeyIndex]);
+  }
+
+});
